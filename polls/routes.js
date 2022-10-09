5bf2688cfe444ce4c21d9557f93622aaae3aae16
Akshar Raaj <raaj.akshar@gmail.com>
2022-10-09 12:12:51 +0530

    Added polls detail route. If the id doesn't exist in the database, then respond with status code 400

--- polls/routes.js
+++ polls/routes.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const {Question} = require('./models')
 
 const pollsIndex = function (req, res) {
   res.send("Hello, world. You're at the polls index.")
 }
 
+const pollsDetail = async (req, res) => {
+  const pollId = req.params.pollId
+  const question = await Question.findByPk(pollId)
+  if (question === null) {
+    res.sendStatus(404)
+    return
+  }
+  res.send(`You're looking at question ${pollId}. The question text is ${question.questionText}`)
+}
+
 router.get('/', pollsIndex)
+router.get('/:pollId/', pollsDetail)
 
 module.exports = router
