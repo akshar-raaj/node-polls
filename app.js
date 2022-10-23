cfa05f4fee2923f379d62898f60240aae79cafa8
Akshar Raaj <raaj.akshar@gmail.com>
2022-10-24 01:30:44 +0530

    Refactor

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const polls = require('./polls/routes')
 const {homeHandler} = require('./handlers')
+
 const app = express()
-const port = 3000
 
 app.get('/', homeHandler)
 app.use('/polls', polls)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app
