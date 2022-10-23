36e00cc4b316d430bd830c021a0ebdcb4650f1d3
Akshar Raaj <raaj.akshar@gmail.com>
2022-10-24 01:52:09 +0530

    Read database credentials from a .env file using package dotenv.

--- constants.js
+++ constants.js
@@ -1,10 +1,12 @@
 "use strict"
+const dotenv = require('dotenv')
+dotenv.config()
 const DATABASE = {
-  NAME: 'polls',
-  USER: 'root',
-  PASSWORD: 'Up123pU!',
-  HOST: '127.0.0.1',
-  PORT: 3306
+  NAME: process.env.DATABASE_NAME,
+  USER: process.env.DATABASE_USER,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  HOST: process.env.DATABASE_HOST,
+  PORT: process.env.DATABASE_PORT
 }
 
 module.exports = {'DATABASE': DATABASE}
