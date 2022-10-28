09f5109f055d1d30ec4a2d3ab08dfba22d13a890
Akshar Raaj <raaj.akshar@gmail.com>
2022-10-28 16:38:06 +0530

    Moved handler/controller to a separate file

--- polls/routes.js
+++ polls/routes.js
@@ -1,27 +1,9 @@
 "use strict"
+
 const express = require('express')
 const router = express.Router()
-const {Question} = require('./models')
-const pug = require('pug')
 
-const pollsIndex = async function (req, res) {
-  const compiledFunction = pug.compileFile('./polls/templates/index.pug')
-  let questions = await Question.findAll({order: [['pubDate', 'ASC']], limit: 5})
-  const body = compiledFunction({questions: questions})
-  res.send(body)
-}
-
-const pollsDetail = async (req, res) => {
-  const pollId = req.params.pollId
-  const question = await Question.findByPk(pollId)
-  if (question === null) {
-    res.sendStatus(404)
-    return
-  }
-  const compiledFunction = pug.compileFile('./polls/templates/detail.pug')
-  const body = compiledFunction({question: question})
-  res.send(body)
-}
+const {pollsIndex, pollsDetail} = require('./handlers')
 
 router.get('/', pollsIndex)
 router.get('/:pollId/', pollsDetail)
