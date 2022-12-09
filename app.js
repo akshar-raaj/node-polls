ec71acdbcb228ea6959cd3685de80ea1d2c1f738
Akshar Raaj <raaj.akshar@gmail.com>
2022-12-09 19:40:43 +0530

    Added a middleware to log requests

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 "use strict"
 const express = require('express')
+
+const bunyan = require('bunyan')
+const log = bunyan.createLogger({'name': 'app'})
+
 const polls = require('./polls/routes')
 const {homeHandler} = require('./handlers')
 
 const app = express()
+
+const logRequest = function (req, res, next) {
+  log.info({'event': 'request', 'path': req.path})
+  next()
+}
+
+app.use(logRequest)
 
 app.get('/', homeHandler)
 app.use('/polls', polls)
