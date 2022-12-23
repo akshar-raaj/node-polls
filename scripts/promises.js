f469effc134a9b36c83a3a23de37c3032c00fe3d
Akshar Raaj <akshar@urbanpiper.com>
2022-12-24 00:23:49 +0530

    Added another promise

--- scripts/promises.js
+++ scripts/promises.js
@@ -4,4 +4,12 @@ const createPromise = (value) => {
   })
 }
 
+const sayValue = function(value, milliSeconds) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve(value)}, milliSeconds)
+  })
+  return promise
+}
+
 exports.createPromise = createPromise
+exports.sayValue = sayValue
