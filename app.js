59d3f73ce935bf58f5c836b0b6216e4f7cccf435
Akshar Raaj <akshar@urbanpiper.com>
2022-12-25 01:31:20 +0530

    Modified logger name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 
 const bunyan = require('bunyan')
-const log = bunyan.createLogger({'name': 'app'})
+const logger = bunyan.createLogger({'name': 'app'})
 
 const polls = require('./polls/routes')
 const {homeHandler} = require('./handlers')
@@ -11,9 +11,8 @@ const test = require('./test/routes')
 const app = express()
 
 const logRequest = function (req, res, next) {
-  log.info({'event': 'request', 'path': req.path})
+  logger.info({'event': 'request', 'path': req.path})
   next()
-  log.info({'event': 'request_done', 'path': req.path})
 }
 
 app.use(logRequest)
