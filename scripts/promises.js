18b1c57ddbc1f091cb11b1da42c11a7845820d2c
Akshar Raaj <akshar@urbanpiper.com>
2023-01-03 18:38:48 +0530

    More verbose explanation around promises

--- scripts/promises.js
+++ scripts/promises.js
@@ -5,11 +5,34 @@ const createPromise = (value) => {
 }
 
 const sayValue = function(value, milliSeconds) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {resolve(value)}, milliSeconds)
-  })
+  // Resolves the value after x milliseconds
+
+  const executor = function (resolve, reject) {
+    const callback = function () {
+      if (value.length <= 5) {
+        resolve(value)
+      }
+      else {
+        reject(new Error(value))
+      }
+    }
+    setTimeout(callback, milliSeconds)
+  }
+  const promise = new Promise(executor)
   return promise
+}
+
+const invokeSayValue = function (value) {
+  let p = sayValue(value, 5000)
+  const success = function (data) {
+    console.log(data)
+  }
+  const failure = function (error) {
+    console.log(error)
+  }
+  p.then(success, failure)
 }
 
 exports.createPromise = createPromise
 exports.sayValue = sayValue
+exports.invokeSayValue = invokeSayValue
